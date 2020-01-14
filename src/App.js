0096ae99880688d4b01218f70a364625b5604ac5
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-15 00:51:21 +0100

    tweaked disabled class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Calendar from './Components/Calendar/Calendar';
 
 const App = () => {
   return (
     <div className="App">
-      
+      <Calendar />
     </div>
   );
 }
