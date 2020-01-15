9b5eaf31c84f8dacefd3b26950e431fda1530278
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-15 15:37:00 +0100

    added contact data components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import reducer from './store/reducer';
+import reducer from './store/reducer-1';
 
 const store = createStore(reducer);
 
