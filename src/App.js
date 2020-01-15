34a9b1d28af4e540b5560e70672dabf856851095
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-15 01:11:53 +0100

    fixing footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Calendar from './Components/Calendar/Calendar';
 
 const App = () => {
   return (
-    <div className="App">
-      <Calendar />
+    <div className="app">
+      <div className="app-card">
+      <div className="footer"></div>
+      </div>
+      {/* <Calendar /> */}
+
+      
     </div>
   );
 }
