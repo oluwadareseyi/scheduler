13af5cb5cd1bd9bd519bbcd6f68666b2a13e1424
oluwadareseyi <oluwadareseyii@gmail.com>
2020-01-15 15:42:38 +0100

    added component body and css

---
+++ src/Components/ContactData/ContactData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./ContactData.scss";
+import Input from "../UI/Input/Input"
+
+const ContactData = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ContactData;
---
+++ src/Components/UI/Input/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Input.scss"
+
+const Input = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Input;
